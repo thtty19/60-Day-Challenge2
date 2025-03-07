7bbd0199d89e66cfd7f2ff272d728f6add615afa
thtty19 <thturner19@gmail.com>
2025-03-06 20:50:35 -0500

    committing the training code that I have written so far to the repository. Closing up for the day

---
+++ main.js
@@ -0,0 +1,19 @@
+const todos = [
+    {
+        id: 1,
+        text: 'Take out trash',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'Meeting with boss',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: 'Denstist appointment',
+        isCompleted: false
+    },
+]
+console.log(todos[1].text);
+// printing out just "meeting with boss" in the console
